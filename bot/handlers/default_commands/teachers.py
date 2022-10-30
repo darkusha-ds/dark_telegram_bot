43b987c09d6e1e4b5f9c209011dd4030c61eeaaf
darkusha-ds <mts04@inbox.ru>
2022-10-30 21:03:06 +0500

    upd

--- bot/handlers/default_commands/teachers.py
+++ bot/handlers/default_commands/teachers.py
@@ -6,6 +6,6 @@ from utils.phrazes import *
 from utils.func import *
 
 
-@dp.message_handler(commands=teacher)
+@dp.message_handler(commands=teachers)
 async def teachers(message: types.Message):
     await message.answer("Выберите нужного преподавателя из списка", reply_markup=teachers_key)
